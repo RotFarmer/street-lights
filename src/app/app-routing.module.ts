77c07a44337d21ee4c6d7c176d4f0c56bdbf3b2e
Rachel Polant <ruchelah@gmail.com>
2020-12-16 15:06:43 -0500

    made blog page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { BlogComponent } from './blog/blog.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
   { path: 'about', component: AboutPageComponent },
+  { path: 'blog', component: BlogComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' }, // redirect to `first-component`
   // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, MainComponent, AboutPageComponent],
+  declarations: [AppComponent, HeaderComponent, MainComponent, AboutPageComponent, BlogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
